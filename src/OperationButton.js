e0ed6451474ce7a6a056b090ebe462420dc62693
inkxk_ <nokink@hotmail.com>
2022-02-03 06:32:25 +0700

    master

---
+++ src/OperationButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ACTIONS } from "./App";
+
+export default function DigitButton({ dispatch, operation }) {
+    return (
+        <button
+            onClick={() =>
+                dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+            }
+        >
+            {operation}
+        </button>
+    );
+}
